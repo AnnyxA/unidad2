f1a737c3d6aab8f3e3d0600e2c79e7fed5131e06
AnnyxA <143622095+AnnyxA@users.noreply.github.com>
2023-09-06 08:38:43 -0600

    Add files via upload

---
+++ Ltareas.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package variables;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ltareas {
+    public static void main(String[] args) {
+            
+    String Ta1 = "Lavar trastes";
+    String Ta2 = "Lavar ropa";
+    String Ta3 = "Barrer la casa";
+    String Ta4 = "tender camas";
+    String Ta5 = "Cocinar"; 
+   
+            
+  System.out.println("Tus tareas pendientes son " + Ta1);  
+  System.out.println(". " + Ta2);
+  System.out.println(". " + Ta3);
+  System.out.println(". " + Ta4);
+  System.out.println(". " + Ta5);
+  
+  
+
+    }
+}   
+
