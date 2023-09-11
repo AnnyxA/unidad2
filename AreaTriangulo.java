fbd502e3b7e7ff44e8f78cd2b6bf663b38cc1265
AnnyxA <143622095+AnnyxA@users.noreply.github.com>
2023-09-11 08:39:06 -0600

    Add files via upload

---
+++ AreaTriangulo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriangulo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // numeros enteros
+        //numeros decimales
+    }
+    int b = 10
+    int A = 56
+    int area = b*A/2;
+   System.out.println("El area del traingulo es: " + area);
+}
